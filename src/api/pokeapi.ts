8042e66cdb42760abeb16b46497f85e71626fc86
Dario Scrivano <darioyosoy@gmail.com>
2021-11-10 12:27:54 -0300

    axios instance created

--- src/api/pokeapi.ts
+++ src/api/pokeapi.ts
@@ -1,16 +1,24 @@
 import axios from 'axios';
 
-// export const getPokemonByName = async (name: string) => {
-//   const res = await apiInstance().get(`pokemon/${name}`);
-//   return res.data;
-// }
+const axiosInstance = () => axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/',
+  headers: { 'Content-Type': 'application/json' },
+});
 
-// export const getPokemonById = async (id: number) => {
-//   const res = await apiInstance().get(`pokemon/${id}`);
-//   return res.data;
-// }
+export const getPokemonByName = async (name: string) => {
+  const pokeInstance = axiosInstance();
+  const res = await pokeInstance.get(`pokemon/${name}`);
+  return res.data;
+}
 
-export const getPokemonBatch = async (quantity: number, startIdx:number) => {
-  const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${quantity}&offset=${startIdx}`);
+export const getPokemonById = async (id: number) => {
+  const pokeInstance = axiosInstance();
+  const res = await pokeInstance.get(`pokemon/${id}`);
+  return res.data;
+}
+
+export const getPokemonBatch = async (quantity: number, startIdx: number) => {
+  const pokeInstance = axiosInstance();
+  const res = await pokeInstance.get(`pokemon?limit=${quantity}&offset=${startIdx}`);
   return res.data;
 }
